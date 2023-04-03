a744b1202644f81ff4975e636b54a0bd3c73fb06
v-Sharan <vsharansago@gmail.com>
2023-04-03 21:56:24 +0530

    first commit

---
+++ client/app/index.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "red", fontSize: 100 }}>Todo</Text>
+    </View>
+  );
+};
+
+export default index;
